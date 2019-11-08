636f31924b50662e51030d4a0a809492e6ee1ad6
barasher <arnaud.barasher@gmail.com>
2019-11-08 23:15:54 +0100

    Handles multiple date recovering

--- recover.sh
+++ recover.sh
@@ -1,28 +1,3 @@
 #! /bin/bash
 
-rm -rf work/*
-
-luser=${LTI_LINKY_USER?"No Linky username specified"}
-lpass=${LTI_LINKY_PASS?"No Linky password specified"}
-iurl=${LTI_IDB_URL?"No InfluxDB URL specified"}
-idb=${LTI_IDB_DB?"No database specified"}
-iloc=${LTI_IDB_LOC?"No localisation specified"}
-
-python3 linkyCrawler.py -u $luser -p $lpass -d $1 > work/data.json
-status=$?
-if [ $status -ne 0 ]; then
-    exit 1
-fi
-
-python3 jsonToLineProtocol.py -s work/data.json -l $iloc -d $1 > work/data.txt
-status=$?
-if [ $status -ne 0 ]; then
-    exit 1
-fi
-
-./pusher -t 30s -u $iurl -d $idb -f work/data.txt
-#cat work/data.txt
-status=$?
-if [ $status -ne 0 ]; then
-    exit 1
-fi
+python3 recover.py -d $1 -c $2
